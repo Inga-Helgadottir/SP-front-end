e3fc5ab2c575afb5016e1334e7f5094011c28c73
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-05-05 13:45:03 +0200

    added comments

--- src/settings.js
+++ src/settings.js
@@ -1,1 +1,6 @@
 export const loginUrl = "https://test.singah.dk/devops-starter/api/login";
+// http://localhost:8080/SP_back_end_war_exploded/api/info/cocktails/random
+// remember to replace vodka with a variable http://localhost:8080/SP_back_end_war_exploded/api/info/cocktails/ingridient/vodka
+// remember to replace margarita with a variable http://localhost:8080/SP_back_end_war_exploded/api/info/cocktails/name/margarita
+// remember to replace a with a variable http://localhost:8080/SP_back_end_war_exploded/api/info/cocktails/letter/a
+// not working yet: http://localhost:8080/SP_back_end_war_exploded/api/info/cocktails/all
