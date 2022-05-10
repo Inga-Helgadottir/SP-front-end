d7f1e59a39974de54b95b84ff401dacdd9913e90
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-05-10 14:39:28 +0200

    see all cocktails done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 // add the logo to the images folder and change the path here to the logo here
-import img from "../images/glass.jpg";
+// import img from "../images/glass.jpg";
 
 const Header = () => {
   return (
     <header>
       <h1>This is our CA 2 project</h1>
-      <img src={img} alt="JAIM Logo" />
+      {/* <img src={img} alt="JAIM Logo" /> */}
       {/* https://www.w3schools.com/cssref/pr_background-image.asp */}
     </header>
   );
