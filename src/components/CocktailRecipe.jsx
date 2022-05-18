9cbab85cc426f65ab956967c7a4e5173f7eff3bb
GoodOldNin <71062853+GoodOldNin@users.noreply.github.com>
2022-05-18 10:16:41 +0200

    Works again

--- src/components/CocktailRecipe.jsx
+++ src/components/CocktailRecipe.jsx
@@ -1,14 +1,42 @@
 import React from "react";
+import { Cocktail } from "./Cocktail";
+import { CocktailDB } from "./CocktailDB";
+import { getCocktailByIdAPIUrl } from "../settings";
+import { getCocktailByIdUrl } from "../settings";
 
 const CocktailRecipe = () => {
   let currentUrl = window.location.href;
   let urlArray = currentUrl.split("/");
   const currentIndex = urlArray[4];
 
+const getCocktailByIdAPI = async () => {
+  const res = await fetch(getCocktailByIdAPIUrl + currentIndex);
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+
+const getCocktailById = async () => {
+  const res = await fetch(getCocktailByIdUrl + currentIndex);
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+if (currentIndex > 10000) {
+  getCocktailByIdAPI();
+} else {
+  getCocktailById();
+}
+
+ 
+
   return (
+    
     <div>
       <p>tester {currentIndex}</p>
+      
     </div>
+    
   );
 };
 
--- src/settings.js
+++ src/settings.js
@@ -6,6 +6,10 @@ export const allCocktailsDB =
   "http://localhost:8080/SP_back_end_war_exploded/api/info/cocktails/all";
 export const makeCocktailUrl =
   "http://localhost:8080/SP_back_end_war_exploded/api/info/cocktails/add";
+  export const getCocktailByIdAPIUrl =
+  "http://localhost:8080/SP_back_end_war_exploded/api/info/cocktails/API/";
+  export const getCocktailByIdUrl =
+  "http://localhost:8080/SP_back_end_war_exploded/api/info/cocktails/";
 // remember to replace 11007 with a variable http://localhost:8080/SP_back_end_war_exploded/api/info/cocktails/API/11007
 // http://localhost:8080/SP_back_end_war_exploded/api/info/cocktails/random
 // remember to replace vodka with a variable http://localhost:8080/SP_back_end_war_exploded/api/info/cocktails/ingridient/vodka
